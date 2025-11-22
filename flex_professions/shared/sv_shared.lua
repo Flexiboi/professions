416d26578835c7f89d78a97e208f667216782e0a
Flex <45070800+Flexiboi@users.noreply.github.com>
2025-11-22 02:22:37 +0100

    Reformat SV_Config initialization in sv_shared.lua

--- flex_professions/shared/sv_shared.lua
+++ flex_professions/shared/sv_shared.lua
@@ -2,8 +2,9 @@ SV_Config = {}
 
 SV_Config.Professions = Config.Professions
 SV_Config.Webhooks = {
-    police = {role = "", hook = "", color = "blue"},
+    police = {role = "", hook = "YOUR_WEBHOOK_URL_HERE", color = "blue"},
     ambulance = {role = "", hook = "YOUR_WEBHOOK_URL_HERE", color = "yellow"},
     mechanic = {role = "", hook = "YOUR_WEBHOOK_URL_HERE", color = "red"},
 
 }
+
